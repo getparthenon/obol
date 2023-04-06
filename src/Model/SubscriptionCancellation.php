5301c84af2763c54d382e0408e0bce243835595e
Iain <iain@humblyarrogant.io>
2023-04-06 13:30:15 +0000

    [Obol] Improve subscription cancel
    Automatic commit for https://github.com/getparthenon/monorepo/commit/8c8bebe4ada9d5df24e63c1b20f175038eb6ccdb

---
+++ src/Model/SubscriptionCancellation.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright Iain Cambridge 2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://github.com/getparthenon/obol/blob/main/LICENSE.
+ *
+ * Change Date: TBD ( 3 years after 1.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace Obol\Model;
+
+class SubscriptionCancellation
+{
+    private Subscription $subscription;
+
+    private ?Refund $refund = null;
+
+    public function getSubscription(): Subscription
+    {
+        return $this->subscription;
+    }
+
+    public function setSubscription(Subscription $subscription): void
+    {
+        $this->subscription = $subscription;
+    }
+
+    public function getRefund(): ?Refund
+    {
+        return $this->refund;
+    }
+
+    public function setRefund(?Refund $refund): void
+    {
+        $this->refund = $refund;
+    }
+}
